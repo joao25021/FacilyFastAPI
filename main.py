475adfa55a4341fe56a6eca269a37ae402f00659
joao25021 <40729448+joao25021@users.noreply.github.com>
2021-11-02 19:50:22 -0300

    aws4

--- main.py
+++ main.py
@@ -175,6 +175,6 @@ def delete_users(contato_id: int, db: Session = Depends(get_db)):
 
 
 if __name__ == '__main__':
-    uvicorn.run(app, host="0.0.0.0",port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
 handler = Mangum(app=app)
