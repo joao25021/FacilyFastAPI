232a6cc05ef78beda3e6bb3ee8d3115a524aefba
joao25021 <40729448+joao25021@users.noreply.github.com>
2021-11-03 21:17:05 -0300

    data

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/contatos2"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/contatos2"
 
 
 db_engine = create_engine(SQLALCHEMY_DATABASE_URL)
