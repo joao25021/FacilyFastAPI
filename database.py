283e1dff47414b0b3db1a660dfe1bb212536eaeb
joao25021 <40729448+joao25021@users.noreply.github.com>
2021-11-05 01:10:31 -0300

    melhoria no banco

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/contatos2"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:root@localhost:3306/contatos"
 
 
 db_engine = create_engine(SQLALCHEMY_DATABASE_URL)
