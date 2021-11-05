d250359a27427a5c2d9608e72fb9ebe9c101c3a9
joao25021 <40729448+joao25021@users.noreply.github.com>
2021-11-05 01:00:48 -0300

    test melhorados -doc -list -get_contato

--- test/test_main.py
+++ test/test_main.py
@@ -4,12 +4,21 @@ from main import app
 client = TestClient(app)
 
 
+def test_main_get_docs():
+    response = client.get("/docs")
+    assert response.status_code == 200
+
+
+def test_main_get_redoc():
+    response = client.get("/redoc")
+    assert response.status_code == 200
+
+
 def test_main_get_list():
     response = client.get("/contatos/?skip=0&limit=100")
     assert response.status_code == 200
 
 
-
-
-
-
+def test_main_get_contato():
+    response = client.get("/contatos/10")
+    assert response.status_code == 404
