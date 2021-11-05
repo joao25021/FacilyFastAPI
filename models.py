b6461dd5417698eca12f172b9051fb3a349d4453
joao25021 <40729448+joao25021@users.noreply.github.com>
2021-11-05 01:07:38 -0300

    alteração de tipos de atributos

--- models.py
+++ models.py
@@ -3,10 +3,10 @@ from sqlalchemy import Column, Integer, String
 from database import Base
 
 
-class Contato(Base):
+class Contato(Base)
     __tablename__ = "contatos"
     id = Column(Integer, primary_key=True, index=True)
-    nome = Column(String(25), unique=True)
+    nome = Column(String(25))
     sobrenome = Column(String(25))
     numerodetelefone = Column(Integer)
     email = Column(String(50), unique=True, index=True)
